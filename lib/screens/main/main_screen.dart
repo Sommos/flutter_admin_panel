3ed625dd068c0fae2330b53d18609df8414aea97
Sommos <Sommosgaming@gmail.com>
2023-05-02 17:33:33 +0100

    added drawer to main_screen.dart

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -9,12 +10,56 @@ class MainScreen extends StatelessWidget {
           children: [
             Expanded(
               // default flex = 1, therefore this takes 16.6% of the screen
-              child: Column(
-                children: [
-                  DrawerHeader(
-                    child: Image.asset("assets/images/logo.png"),
-                  )
-                ],
+              child: Drawer(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      DrawerHeader(
+                        child: Image.asset("assets/images/logo.png"),
+                      ),
+                      DrawerListTile(
+                        title: "Dashboard", 
+                        svgScource: "assets/icons/menu_dashbord.svg", 
+                        press: (){},
+                      ),
+                      DrawerListTile(
+                        title: "Transaction",
+                        svgScource: "assets/icons/menu_tran.svg", 
+                        press: (){},
+                      ),
+                      DrawerListTile(
+                        title: "Task",
+                        svgScource: "assets/icons/menu_task.svg", 
+                        press: (){},
+                      ),
+                      DrawerListTile(
+                        title: "Documents",
+                        svgScource: "assets/icons/menu_doc.svg", 
+                        press: (){},
+                      ),
+                      DrawerListTile(
+                        title: "Store",
+                        svgScource: "assets/icons/menu_store.svg", 
+                        press: (){},
+                      ),
+                      DrawerListTile(
+                        title: "Notification",
+                        svgScource: "assets/icons/menu_notification.svg", 
+                        press: (){},
+                      ),
+                      DrawerListTile(
+                        title: "Profile",
+                        svgScource: "assets/icons/menu_profile.svg", 
+                        press: (){},
+                      ),
+                      DrawerListTile(
+                        title: "Settings",
+                        svgScource: "assets/icons/menu_setting.svg", 
+                        press: (){},
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
             Expanded(
@@ -25,6 +70,41 @@ class MainScreen extends StatelessWidget {
               ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class DrawerListTile extends StatelessWidget {
+  const DrawerListTile({
+    super.key, 
+    required this.title, 
+    required this.svgScource, 
+    required this.press,
+  });
+
+  final String title;
+  final String svgScource;
+  final VoidCallback press;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: press,
+      horizontalTitleGap: 0.0,
+      leading: SvgPicture.asset(
+        svgScource,
+        height: 16,
+        colorFilter: const ColorFilter.mode(
+          Colors.white54, 
+          BlendMode.srcIn,
+        ),
+      ), 
+      title: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.white54,
         ),
       ),
     );
